16b72484b9379fa9df227833f067d9ebe7676855
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-12-06 13:44:07 -0500

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -5,24 +5,28 @@ const list_players = document.getElementById("lists_of_games");
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
   import { getAuth, onAuthStateChanged, signInAnonymously, setPersistence, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
   import { getDatabase, set, ref, onValue } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyDkf2Xme8vIYwSjNgpikMPlHETkteqEsfI",
-    authDomain: "tic-tac-toe-online-108ca.firebaseapp.com",
-    databaseURL: "https://tic-tac-toe-online-108ca-default-rtdb.firebaseio.com",
-    projectId: "tic-tac-toe-online-108ca",
-    storageBucket: "tic-tac-toe-online-108ca.appspot.com",
-    messagingSenderId: "971654471519",
-    appId: "1:971654471519:web:3e05a829c5db81a4f920ea"
-  };
+  apiKey: "AIzaSyB-aP1AEjzCLPCs7Yg4GY_Z4pnLZ8T2qys",
+  authDomain: "ping-pong-online.firebaseapp.com",
+  projectId: "ping-pong-online",
+  storageBucket: "ping-pong-online.appspot.com",
+  messagingSenderId: "213821744223",
+  appId: "1:213821744223:web:d86dbc68386c81a10567ea",
+  measurementId: "G-PXQC20J5RZ"
+};
+
+
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const database = getDatabase();
+  const analytics = getAnalytics(app);
   let playerId;
   let playerRef; 
   onAuthStateChanged(auth, (user) => {
