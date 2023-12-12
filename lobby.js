2db23a27d10b5e91b50af5e193a1184c0bda79c6
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-12-12 00:20:53 -0500

    Create lobby.js

---
+++ lobby.js
@@ -0,0 +1,118 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { getDatabase, set, ref, onValue, get,child,remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAjHsCstknxm_3cdBzYulCBSZLxSr1qqJQ",
+    authDomain: "ping-pong-online-4248e.firebaseapp.com",
+    projectId: "ping-pong-online-4248e",
+    storageBucket: "ping-pong-online-4248e.appspot.com",
+    messagingSenderId: "967048086805",
+    appId: "1:967048086805:web:2b5d4563794ed5934ed51c",
+    measurementId: "G-RBV9QDF1HP"
+  };
+
+function add_player_2(a) { // adds player 2 to database
+	set(ref(database, "/games/" + gameId + "/players"), a);
+ }
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase();
+const dbRef = ref(getDatabase());
+
+function random_number_gen() {
+	return Math.floor(Math.random() * 9999);
+}
+
+var playerId;
+var opponentId;
+var gameId;
+const searchParams = new URLSearchParams(window.location.search);
+if (searchParams.get('game_id') == "new") {
+  gameId = random_number_gen();
+}
+else {
+  gameId = searchParams.get('game_id');
+}
+
+
+onAuthStateChanged(auth, (user) => {
+	if(user) {
+		playerId = user.uid;
+		console.log("User is signed in");
+		console.log(playerId);
+		document.getElementById("user_id").innerHTML += playerId;
+		document.getElementById("game_id").innerHTML = gameId;
+		const gamesRef = ref(database, 'games/' + gameId);
+		onValue(gamesRef, (snapshot) => {
+			var data = snapshot.val();
+			console.log(data);
+	 		if (data == null && game_start == true) {
+				console.log("game null");
+				window.location.href = "https://jcamille2023.github.io/pong/index?game_removed=true";
+			}
+		});
+    if(searchParams.get('game_id') == "new") {
+		set(ref(database, "/games/" + gameId + "/players"), {
+		player_1: playerId,
+    		});
+		var playersRef = ref(database, "/games/" + gameId + "/players");
+		onValue(playersRef, (snapshot) => {
+			 const data = snapshot.val();
+			 console.log(data);
+			 if (data.player_2) {
+				 opponentId = data.player_2;
+				 document.getElementById("opponent_id").innerHTML += opponentId;
+         document.getElementById("start_button").setAttribute("style","");
+			 }
+		});
+   }
+    else {
+      get(child(dbRef,"games/" + gameId + "/players")).then((snapshot) => {
+        data = snapshot.val()
+        data.player_2 = playerId;
+        add_player_2(data);
+      });
+    }
+    var startRef = ref(database, "/games/" + gameId);
+		onValue(playersRef, (snapshot) => {
+			 const data = snapshot.val();
+			 console.log(data);
+			 if (data.game_start == true) {
+				 var url = new URL("https://jcamille2023.github.io/pong/game");
+         url.searchParams.append('game_id', gameId);
+         console.log(url);
+         window.location.href = url;
+			 }
+		});
+  }
+	else {
+		console.log("User is signed out");
+	}
+});
+
+function start_game() {
+  var data;
+  get(child(dbRef, "games/" + gameId)).then((snapshot) => {
+    data = snapshot.val();
+    data.game_start = true;
+  });
+  set(ref(database, "games/" + gameId), data);
+}
+
+
+function delete_session() {
+	let game_ref = ref(database, "/games/" + gameId);
+	remove(game_ref);
+	console.log("Game session deleted.");
+}
+
+window.start_game = start_game;
+window.delete_session = delete_session;
+
+
+
+
